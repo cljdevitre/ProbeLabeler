22c994f4984139954bb26803853729fe4dfd9255
cljdevitre <103474333+cljdevitre@users.noreply.github.com>
2023-09-12 14:10:10 -0700

    setup files

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+__author__ = 'Charlotte Devitre'
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# This is for filtering cosmic rays
+from ProbeLabeler.jeol_labeler import *
+
+# version
+from ._version import __version__
+
+
+
